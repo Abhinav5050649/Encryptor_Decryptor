15905fc2bfabbcd0ba66072faf148b4b2d88b421
Abhinav Sharma <abhinav291sharma@gmail.com>
2022-07-18 00:41:12 +0530

    Finished edf.py and modified .gitignore

--- edf.py
+++ edf.py
@@ -19,80 +19,139 @@ def main():
 
         if (e_or_d == 0):
             break
-        # e_or_d = int(input("Press 1 to continue. Press 0 to exit"))
+        elif (e_or_d == 1):
+            encrypter()
+        elif (e_or_d == 2):
+            decrypter()        
+
+def encrypter():
+    en_ch = int(input("Press:\n1: Morse Code\n2:SHA256\n3:Fernet:\n4:RSA\n\nEnter choice:-"))
+
+    if (en_ch == 1):
+        
+        iofname = input("Enter name of input file: ")
+        iof = open(iofname, "r")
+        oofname = input("Enter name of output file: ")
+        oof = open(oofname, "w")
+
+        sentences = iof.read()
+        sentences = sentences.upper()
+        morse_word = morse_lookup(sentences)
+        oof.write(morse_word)
+
+        iof.close()
+        oof.close()
+
+    elif (en_ch == 2):
+
         iofname = input("Enter name of input file: ")
         iof = open(iofname, "r")
         oofname = input("Enter name of output file: ")
         oof = open(oofname, "w")
         
-        # encrypter(iof, oof)
+        sha_input_text = iof.read() 
+        sha_output_text = sha_encrypt(sha_input_text)
+        oof.write(sha_output_text)
 
-        # e_or_d = int(input("Press 1 to continue. Press 0 to exit"))
-
-        if (e_or_d == 1):
-            encrypter(iof, oof)
-        elif (e_or_d == 2):
-            decrypter(iof, oof)        
-
-def encrypter(iof, oof):
-    en_ch = int(input("Press:\n1: Morse Code\n2:SHA256\n3:Fernet:\n4:RSA\n\nEnter choice:-"))
-
-    if (en_ch == 1):
-        
-        for sentences in iof:
-            sentences = sentences.upper()
-            morse_word = morse_lookup(sentences)
-            oof.write(morse_word)
-
-    elif (en_ch == 2):
-        for sha_input_text in iof:
-            sha_output_text = sha_encrypt(sha_input_text)
-            oof.write(sha_output_text)
+        iof.close()
+        oof.close()
 
     elif (en_ch == 3):
-        choice = input("Press Y if you want fernet to generate key. Else, press N if you want to generate key: ")
 
-        if (choice == 'Y'):
-            key = Fernet.generate_key()
-            print("Your fernet key(remember this!!!) is: ", key)
-        else:
-            key = input("Enter key: ")
+        key = Fernet.generate_key()
         
+        name = input("Enter name of file where you wish to store key(without extensions): ")
+        fname = name + ".key"
+        file = open(fname, 'wb')
+        file.write(key)
+        file.close()
+
+        iofname = input("Enter name of input file: ")
+        iof = open(iofname, 'r')
+        oofname = input("Enter name of output file: ")
+        oof = open(oofname, 'wb')
+
         fernet_encrypt(iof, oof, key)
+        iof.close()
+        oof.close()
 
     elif (en_ch == 4):
-        length = int(input("Enter length of rsa keys (Minimum length = 16): "))
-        publicKey, privateKey = rsa.newKeys(length)
-        
-        print("Remember the following keys:\nRSA Public Key: ", publicKey, "\nRSA Private Key: ", privateKey, "\n")
-        for rsa_input_text in iof:
-            rsa_output_text = rsa_encrypt(rsa_input_text, publicKey)
-            oof.write(rsa_output_text)
 
-def decrypter(iof, oof):
+        iofname = input("Enter name of input file: ")
+        iof = open(iofname, "r")
+        oofname = input("Enter name of output file: ")
+        oof = open(oofname, "wb")
+
+        publicKey, privateKey = rsa.newkeys(1024)
+        
+        with open('public_key.pem', 'wb') as f:
+            f.write(publicKey.save_pkcs1('PEM'))
+
+        with open('private_key.pem', 'wb') as f:
+            f.write(privateKey.save_pkcs1('PEM'))
+
+        print(f"Public key is stored in: public_key.pem and Private key is stored in private_key.pem")
+
+        rsa_input_text = iof.read()
+        rsa_output_text = rsa_encrypt(rsa_input_text, publicKey)
+        oof.write(rsa_output_text)
+
+        iof.close()
+        oof.close()
+
+def decrypter():
     de_ch = int(input("Press:\n1: Morse Code\n2:Fernet:\n3:RSA\n\nEnter choice:-"))
 
     if (de_ch == 1): 
         
-        for morse_sentences in iof:
-            normal_word = normal_lookup(morse_sentences)
-            oof.write(normal_word)
+        iofname = input("Enter name of input file: ")
+        iof = open(iofname, "r")
+        oofname = input("Enter name of output file: ")
+        oof = open(oofname, "w")
+
+        morse_sentences = iof.read()
+        normal_word = normal_lookup(morse_sentences)
+        oof.write(normal_word)
+
+        iof.close()
+        oof.close()
 
     elif (de_ch == 2):
         
-        norm_key = input("Enter the key: ")
-        
-        for norm_fern_input_text in iof:
-            norm_fern_output_text = fernet_decrypt(norm_fern_input_text, norm_key)
-            oof.write(norm_fern_output_text)
+        norm_key_file = input("Enter the file containing the key: ")
+        file = open(norm_key_file, 'rb')
+        norm_key = file.read()
+        file.close()
+
+        iofname = input("Enter name of input file: ")
+        iof = open(iofname, 'rb')
+        oofname = input("Enter name of output file: ")
+        oof = open(oofname, 'w')
+
+        norm_fern_input_text = iof.read()
+        norm_fern_output_text = fernet_decrypt(norm_fern_input_text, norm_key)
+        oof.write(norm_fern_output_text)
+
+        iof.close()
+        oof.close()
 
     elif (de_ch == 3):
 
-        norm_privateKey = input("Enter private key of RSA: ")
-        
-        for norm_rsa_input_text in iof:
-            norm_rsa_output_text = rsa_decrypt(norm_rsa_input_text, norm_privateKey)
-            oof.write(norm_rsa_output_text)    
+        iofname = input("Enter name of input file: ")
+        iof = open(iofname, "rb")
+        oofname = input("Enter name of output file: ")
+        oof = open(oofname, "w")
+
+        privateKeyRSAFile = input("Enter file containing Private Key used to encrypt file: ")
+        with open(privateKeyRSAFile, 'rb') as f:
+            norm_privateKey = rsa.PrivateKey.load_pkcs1(f.read())
+            
+        norm_rsa_input_text = iof.read()
+        norm_rsa_output_text = rsa_decrypt(norm_rsa_input_text, norm_privateKey)
+        oof.write(norm_rsa_output_text)   
+
+        iof.close()
+        oof.close() 
 
 def morse_lookup(a):
     morse_char_dict = {
@@ -246,17 +305,16 @@ def normal_lookup(a):
     return chars
     
 def sha_encrypt(sha_input):
+
     encrypted = hashlib.sha256(sha_input.encode('utf-8')).hexdigest()
     return encrypted
 
 def fernet_encrypt(iof, oof, key):
-        # f_encrypted = fernet.encrypt(input_text.encode())
-        # return f_encrypted
         
         fernet = Fernet(key)
-        for fernet_input_text in iof:
-            fernet_output_text = fernet.encrypt(fernet_input_text.encode())
-            oof.write(fernet_output_text)
+        fernet_input_text = iof.read()
+        fernet_output_text = fernet.encrypt(fernet_input_text.encode())
+        oof.write(fernet_output_text)
 
 def fernet_decrypt(input_text, key):
     fernet = Fernet(key)
@@ -265,11 +323,12 @@ def fernet_decrypt(input_text, key):
 
 def rsa_encrypt(rsa_input_text, publicKey):
     
-    rsa_encrypted = rsa.encrypt(rsa_input_text.encode(), publicKey)
+    rsa_encrypted = rsa.encrypt(rsa_input_text.encode('ascii'), publicKey)
     return rsa_encrypted
 
 def rsa_decrypt(norm_rsa_input_text, privateKey):
-    rsa_decrypted = rsa.decrypt(norm_rsa_input_text, privateKey).decode()
+
+    rsa_decrypted = rsa.decrypt(norm_rsa_input_text, privateKey).decode('ascii')
     return rsa_decrypted
 
 main()
